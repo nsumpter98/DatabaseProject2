8bfeedff9e0633eaf09d041d4831410662eb601f
Nathanael Sumpter <nsumpter98@gmail.com>
2024-04-02 21:16:41 -0400

    updating requirements

--- DatabaseProject2/settings.py
+++ DatabaseProject2/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "missing_data.apps.MissingDataConfig",
-    "rest_api.apps.RestApiConfig"
+    "rest_api.apps.RestApiConfig",
+
 ]
 
 MIDDLEWARE = [
